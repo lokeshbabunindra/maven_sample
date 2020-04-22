2b9133ec688261199329930e28d3b0fbd2f5559d
LokeshbabuNindra <lokeshbabunidra@gmail.com>
2020-04-23 03:08:13 +0800

    inital commit of sample maven

---
+++ src/main/java/com/execise1/JavaApp.java
@@ -0,0 +1,7 @@
+package com.execise1;
+
+public class JavaApp {
+     public String Sample() {
+    	 return "Sample" ;
+     }
+}
---
+++ src/test/java/com/execise1/TestApp.java
@@ -0,0 +1,12 @@
+package com.execise1;
+
+import org.junit.Test;
+
+public class TestApp {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
